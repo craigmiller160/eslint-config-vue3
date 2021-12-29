b0f9e760a2d768ef4439b17ca4e5b08d9af4785f
Craig <craigmiller160@gmail.com>
2021-12-29 15:57:05 -0500

    Added dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        sourceType: 'module'
+    },
+    extends: [
+        'plugin:vue/vue3-essential'
+    ]
+};
